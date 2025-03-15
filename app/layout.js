381cee6eb03f0939476572d12a34fafb033e055f
AhmadBerri <ahmadberri_1997@hotmail.com>
2025-03-15 12:21:09 +0200

    add parallel routing

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,20 @@
-import './globals.css'
+import "./globals.css";
+import MainHeader from "@/components/main-header";
 
 export const metadata = {
-  title: 'NextJS Course App',
-  description: 'Your first NextJS app!',
+  title: "NextJS Course App",
+  description: "Your first NextJS app!",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div id="page">
+          <MainHeader />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
